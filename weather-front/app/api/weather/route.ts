e2359ccc9ada5898b11e24f8e7e24500241d7a22
Katherine_fox <kate.belyanskaya@gmail..com>
2024-02-09 20:58:05 +0200

    works

--- weather-front/app/api/weather/route.ts
+++ weather-front/app/api/weather/route.ts
@@ -2,11 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 const APIKEY = '5faab37d79a55fd06072e3ada49f3a7a';
 export async function GET(request: any) {
-    // const {searchParams} = new URL(request.url);
-    // const addres = searchParams.get('address');
-
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${APIKEY}`;
+    const {searchParams} = new URL(request.url);
+    const city = searchParams.get('city');
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`;
+   
     const res = await fetch(url);
     const data = await res.json();
+    console.log('city', data)
     return NextResponse.json(data);
 }
--- weather-front/app/page.tsx
+++ weather-front/app/page.tsx
@@ -7,18 +7,25 @@ import { MainContainer } from './styles/global';
 export default function Home() {
   // const date =getCurrentDate();
   const [weatherData, setWeatherData] = useState(null);
-  const [city, setCity] = useState(null);
+  const [city, setCity] = useState('Haifa');
 
   const fetchData = async(cityName:string) => {
     try {
-      const response = await fetch("http://localhost:3000/api/weather/weather?address="+cityName);
+      const response = await fetch(`http://localhost:3000/api/weather/?city=${cityName}`);
+      const data = await response.json();
+      console.log('response', data)
+      setWeatherData(data);
+      
     } catch (err) {
       console.log('err', err)
     }
   }
 
   useEffect(() => {
-    console.log('first')
+    fetchData(city);
+
+    //get icons https://openweathermap.org/weather-conditions
+    // get cities list
   }, [])
   
   return (
